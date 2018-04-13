241eca87a0f2d01bd935f47f658faf3266aa86d1
Abdulrahman Alassadi <alassadi@live.com>
2018-04-13 21:43:15 +0200

    Initial commit

---
+++ app/src/main/java/com/company/overdosesyria/MainActivity.java
@@ -0,0 +1,31 @@
+//Created by Abood Alassadi 13/4/2018
+
+package com.company.overdosesyria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://overdose.mobi");
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return false;
+            }
+        });
+    }
+}
